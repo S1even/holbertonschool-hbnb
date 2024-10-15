f7c68c2ab9265d85e93b6434281d45f58d61c961
Bruqui <dylan2003gd@gmail.com>
2024-10-15 11:19:05 +0200

    Prepare the Configuration

---
+++ part2/hbnb/config.py
@@ -0,0 +1,13 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    DEBUG = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
