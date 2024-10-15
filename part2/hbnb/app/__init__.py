02abb2bf88dc761986b44833fb2f468e2cc273fa
Bruqui <dylan2003gd@gmail.com>
2024-10-15 15:57:13 +0200

    set up Flask application with REST API

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python3
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.users import api as users_ns
 
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
-    # Placeholder for API namespaces (endpoints will be added later)
-    # Additional namespaces for places, reviews, and amenities will be added later
-
+    api.add_namespace(users_ns, path='/api/v1/users')
     return app
