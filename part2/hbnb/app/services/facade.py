de3ae491ece793b33c559300e65a829bcbb621fd
Bruqui <dylan2003gd@gmail.com>
2024-10-16 15:23:13 +0200

    Update code for list all users

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python3
-from app.models.user import User
 from app.persistence.repository import InMemoryRepository
+from app.models.amenity import Amenity
+from app.models.user import User
+
 
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
 
     def create_user(self, user_data):
         user = User(**user_data)
@@ -16,3 +18,6 @@ class HBnBFacade:
 
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
+
+    def get_all_users(self):
+        return self.user_repo.get_all()
