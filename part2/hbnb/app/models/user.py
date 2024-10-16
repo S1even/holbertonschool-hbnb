6a270af17f13dc60a88234cd5e037944b7c07caf
Bruqui <dylan2003gd@gmail.com>
2024-10-16 11:39:34 +0200

    Update the code

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-from .base_model import BaseModel
 import re
+import uuid
 
 
-class User(BaseModel):
-    def init(self, first_name: str, last_name: str, email: str, is_admin=False):
-        super().init()
-        self.first_name = self._validate_name(first_name, "First name", 50)
-        self.last_name = self._validate_name(last_name, "Last name", 50)
-        self.email = self._validate_email(email)
-        self.is_admin = is_admin
+class User:
+    def __init__(self, first_name, last_name, email):
+        self.id = str(uuid.uuid4())
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
 
     def _validate_name(self, name: str, field_name: str, max_length: int) -> str:
         """Validates that the name is not empty and within length limits."""
