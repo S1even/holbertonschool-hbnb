1a9021b39b4bc546e3ffe898787a05cc96f23e3d
Bruqui <dylan2003gd@gmail.com>
2024-10-16 15:20:37 +0200

    add code for list all users

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -30,6 +30,12 @@ class UserList(Resource):
 
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
+    
+    @api.response(200, 'List of amenities retrieved successfully')
+    def get(self):
+        """Retrieve a list of all amenities"""
+        users = facade.get_all_users()
+        return [{"id": user.id, "first_name": user.first_name, "last_name": user.last_name, "email": user.email} for user in users], 200
 
 
 @api.route('/<user_id>')
