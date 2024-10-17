9ade3286252b58b8ae8fbc815f270fbef8acb5e9
S1even <153313481+S1even@users.noreply.github.com>
2024-10-17 10:00:56 +0000

    Update init file for amenity

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -2,10 +2,10 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
-
+from app.api.v1.amenities import api as amenities_ns
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
-
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(amenities_ns, path='/api/v1/amenities')
     return app
