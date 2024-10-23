e9aacb565198231225ed86dabcd9e3e0a93d011e
S1even <8809@holbertonstudents.com>
2024-10-23 13:58:38 +0000

    Add init file

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,11 +1,18 @@
-#!/usr/bin/python3
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.places import api as places_ns
 from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.reviews import api as reviews_ns
+
+
 def create_app():
     app = Flask(__name__)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+    api.add_namespace(places_ns, path='/api/v1/places')
+    api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    
     return app
